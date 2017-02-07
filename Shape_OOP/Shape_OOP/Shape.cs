051745593baa1eedb1a7141aa1e1ac6e3693df7c
hinsonan <hinsonan@gmail.com>
2017-02-07 12:24:18 -0600

    initial commit

---
+++ Shape_OOP/Shape_OOP/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape_OOP
+{
+    abstract class Shape
+    {
+        private int xAxis;
+        private int yAxis;
+        public Shape()
+        {
+            xAxis = 0;
+            yAxis = 0;
+        }
+        public Shape(int x, int y)
+        {
+            xAxis = x;
+            yAxis = y;
+        }
+    }
+}
